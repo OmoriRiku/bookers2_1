376bb557f95779aee4d3aabd3f73dfc280daeb26
OmoriRiku <oomori3321@gmail.com>
2023-06-12 04:49:07 +0000

    本の投稿一覧ページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,15 @@
 class BooksController < ApplicationController
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
   
   def show
@@ -15,5 +22,11 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
